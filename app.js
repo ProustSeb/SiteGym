394dc8f629a14a95c02171a86a119b21cf1b7a8c
Proust Sébastien <sebastienproust77@gmail.com>
2022-02-14 12:02:52 +0100

    RAZ

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ btns.forEach(btn => {
 modaleEquipement.addEventListener('click', () => {
     modaleEquipement.classList.remove('active-modale');
 })
+
+
+// Anim navbar
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 30) {
+        nav.classList.add('anim-nav');
+    } else {
+        nav.classList.remove('anim-nav');
+    }
+
+})
