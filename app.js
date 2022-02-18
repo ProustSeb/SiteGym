149c16fff8eaa90435b8c3546ddc1db05f7844a9
Proust Sébastien <sebastienproust77@gmail.com>
2022-02-18 16:18:46 +0100

    responsive en js

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const btns = document.querySelectorAll('.btn-modale');
 const modaleEquipement = document.querySelector('.bloc-modale');
 const imgIndex = document.querySelector('.bloc-modale img');
 
-btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-
-        imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
-        modaleEquipement.classList.add('active-modale');
-
+if(window.matchMedia("(min-width: 850px)").matches){
+    btns.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+    
+            imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`;
+            modaleEquipement.classList.add('active-modale');
+    
+        })
     })
-})
+    
+    modaleEquipement.addEventListener('click', () => {
+        modaleEquipement.classList.remove('active-modale');
+    })
+}
 
-modaleEquipement.addEventListener('click', () => {
-    modaleEquipement.classList.remove('active-modale');
-})
 
 
 // Anim navbar
@@ -28,4 +31,4 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('anim-nav');
     }
 
-})
+}) 
